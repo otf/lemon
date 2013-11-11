392bc162ec249985117689dea0717eb5a3d8f654
bleis-tift <hey_c_est_la_vie@hotmail.co.jp>
2013-11-11 17:11:25 +0700

    NuGetパッケージ構成の見直し
    Conflicts:
    	Package.nuspec

--- Package.bat
+++ Package.bat
@@ -1,5 +1,4 @@
-set PATH=%PATH%;C:\Windows\Microsoft.NET\Framework\v4.0.30319
-
-
-msbuild solution\Lemon\Lemon.fsproj /p:TargetFramework=v4.0;Configuration=Release;Platform=AnyCPU /t:Rebuild
-nuget pack Package.nuspec
+rem set PATH=%PATH%;C:\Windows\Microsoft.NET\Framework\v4.0.30319
+rem msbuild solution\Lemon\Lemon.fsproj /p:TargetFramework=v4.0;Configuration=Release;Platform=AnyCPU /t:Rebuild
+cd solution\Lemon
+..\.nuget\NuGet.exe pack Lemon.fsproj -Symbols -Build
---
+++ solution/Lemon/AssemblyInfo.fs
@@ -0,0 +1,21 @@
+﻿module AssemblyInfo
+
+open System.Reflection
+open System.Runtime.CompilerServices
+open System.Runtime.InteropServices
+
+[<assembly: AssemblyTitle("Lemon")>]
+[<assembly: AssemblyDescription("This library supports functional programming using F# on ASP.NET.")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCompany("")>]
+[<assembly: AssemblyProduct("Lemon")>]
+[<assembly: AssemblyCopyright("")>]
+[<assembly: AssemblyTrademark("")>]
+[<assembly: AssemblyCulture("")>]
+
+[<assembly: ComVisible(false)>]
+[<assembly: Guid("83425D7B-F5EE-4D74-86B2-D02C1CA566AA")>]
+
+[<assembly: AssemblyVersion("1.3.0.0")>]
+
+()
