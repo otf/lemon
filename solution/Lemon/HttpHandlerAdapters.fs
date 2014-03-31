eab287532e2cc0048a369decc65ab5977414bbbf
otf <otf@me.com>
2014-03-31 20:46:48 +0900

    add HttpHandlerAdapters.

---
+++ solution/Lemon/HttpHandlerAdapters.fs
@@ -0,0 +1,15 @@
+﻿namespace Lemon
+
+open System
+open System.Web
+open Lemon
+  
+module HttpHandlerAdapters =
+  let adapt (handler : #IHttpHandler) (_:Response) = 
+    handler.ProcessRequest(HttpContext.Current)
+    HttpResponseWrapper(HttpContext.Current.Response) :> Response
+
+  let staticFile resp = 
+    let handlerType = (typeof<HttpApplication>).Assembly.GetType("System.Web.StaticFileHandler", true) 
+    let handler = Activator.CreateInstance(handlerType, true) :?> IHttpHandler
+    adapt handler resp
---
+++ solution/Lemon/HttpHandlerAdapters.fsi
@@ -0,0 +1,9 @@
+﻿namespace Lemon
+
+open System
+open System.Web
+open Lemon
+
+module HttpHandlerAdapters =
+  val adapt : #IHttpHandler -> Responser
+  val staticFile : Responser
