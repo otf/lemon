44c0c51310ebd365894586f8474133f13bb15a9a
otf <phoenixsingleton@gmail.com>
2015-03-06 11:27:54 +0900

    .NET4.0と.NET4.5両方に対応するようにする

--- Package.bat
+++ Package.bat
@@ -1,4 +1,5 @@
-rem set PATH=%PATH%;C:\Windows\Microsoft.NET\Framework\v4.0.30319
-rem msbuild solution\Lemon\Lemon.fsproj /p:TargetFramework=v4.0;Configuration=Release;Platform=AnyCPU /t:Rebuild
-cd solution\Lemon
-..\.nuget\NuGet.exe pack Lemon.fsproj -Symbols -Build
+set PATH=%PATH%;C:\Windows\Microsoft.NET\Framework\v4.0.30319
+MSBuild.exe .\solution\Lemon\Lemon.fsproj /t:Rebuild /p:Configuration=Release;Platform=AnyCPU /p:TargetFrameworkVersion=v4.0 /p:OutputPath="..\..\bin\Release\v4.0" /p:VisualStudioVersion=11.0
+MSBuild.exe .\solution\Lemon\Lemon.fsproj /t:Rebuild /p:Configuration=Release;Platform=AnyCPU /p:TargetFrameworkVersion=v4.5 /p:OutputPath="..\..\bin\Release\v4.5" /p:VisualStudioVersion=12.0
+
+.\solution\.nuget\NuGet.exe pack .\solution\Lemon\Lemon.nuspec
